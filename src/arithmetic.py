8a7d03a86397c06274464ae7dc77ac06ab32c7bb
Lukas Bjarre <lbjarre@kth.se>
2018-02-23 21:42:40 +0100

    Fixed a missunderstanding of init params

--- src/arithmetic.py
+++ src/arithmetic.py
@@ -12,21 +12,21 @@ def arithmetic(string, p0):
 
     N = 22
     P = 8
-    
+    p0 = math.floor(p0 * 2**P)
+
     C = 0
-    A = 2**N
+    A = int(2**N)
     r = -1
     b = 0
 
     for s in string:
-        s = int(s)
         T = A * p0
         if s == 1:
             C = C + T
             T = (A << P) - T
 
         if C >= 2**(N + P):
-            C = C % 2**(N + P)
+            C = int(C) & (2**(N + P) - 1)
             encoded += '1'
             if r > 0:
                 encoded += '0' * (r - 1)
@@ -39,7 +39,7 @@ def arithmetic(string, p0):
             T = 2 * T
             C = 2 * C
             if C >= 2**(N + P):
-                C = C % 2**(N + P)
+                C = int(C) & (2**(N + P) - 1)
                 if r < 0:
                     encoded += '1'
                 else:
@@ -49,7 +49,7 @@ def arithmetic(string, p0):
                     encoded += '0' + '1' * r
                 r = 0
 
-        A = math.floor(T / 2**P)
+        A = math.floor(T * 2**(-P))
     
     if r >= 0:
         encoded += '0' + '1' * r
@@ -58,8 +58,7 @@ def arithmetic(string, p0):
     
     return encoded
 
-source = markov_chain(0.9, 0.9, 100)
+source = markov_chain(0.8, 0.8, 19600)
 encoded = arithmetic(source, 0.5)
-print(len(source))
-print(len(encoded))
+print(len(source) / len(encoded))
 
